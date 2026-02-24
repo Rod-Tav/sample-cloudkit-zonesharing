1cd3b8ad67f2cd09b2be10e387c052127c1a5a45
Rod Tavangar <96751400+Rod-Tav@users.noreply.github.com>
2026-02-24 04:46:32 -0800

    some progress

--- ZoneSharing/Views/AddContactView.swift
+++ ZoneSharing/Views/AddContactView.swift
@@ -12,30 +12,40 @@ struct AddContactView: View {
     @State private var phoneInput: String = ""
     @State private var groupInput: String = ""
 
-    /// Callback after user selects to add contact with given name and phone number.
-    let onAdd: ((String, String, String) async throws -> Void)?
-    /// Callback after user cancels.
+    /// What to do after user selects to add contact with given name and phone number.
+    var onAdd: (String, String, String) async throws -> Void = { _,_,_ in }
+    /// What to do after user cancels.
     var onCancel: () -> Void = { }
 
     var body: some View {
-        NavigationView {
-            VStack {
-                TextField("Full Name", text: $nameInput)
-                    .textContentType(.name)
-                
-                TextField("Phone Number", text: $phoneInput)
-                    .textContentType(.telephoneNumber)
-                
-                TextField("Group", text: $groupInput)
+        if #available(iOS 16, *) {
+            NavigationStack {
+                addContactContent
             }
-            .padding()
-            .maxHeight()
-            .navigationTitle("Add Contact")
-            .toolbar {
-                cancellationToolbarContent
-                
-                confirmationToolbarContent
+        } else {
+            NavigationView {
+                addContactContent
             }
+        }
+    }
+    
+    @ViewBuilder private var addContactContent: some View {
+        VStack {
+            TextField("Full Name", text: $nameInput)
+                .textContentType(.name)
+            
+            TextField("Phone Number", text: $phoneInput)
+                .textContentType(.telephoneNumber)
+            
+            TextField("Group", text: $groupInput)
+        }
+        .padding()
+        .maxHeight()
+        .navigationTitle("Add Contact")
+        .toolbar {
+            cancellationToolbarContent
+            
+            confirmationToolbarContent
         }
     }
 }
@@ -54,7 +64,7 @@ extension AddContactView {
         ToolbarItem(placement: .confirmationAction) {
             Button("Save") {
                 Task {
-                    try? await onAdd?(nameInput, phoneInput, groupInput)
+                    try? await onAdd(nameInput, phoneInput, groupInput)
                 }
             }
             .disabled(canSave)
