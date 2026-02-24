3344ec62af47eeea4a566fd6dd878b507488e645
Rod Tavangar <96751400+Rod-Tav@users.noreply.github.com>
2026-02-24 00:35:46 -0800

    error nonsense

--- ZoneSharing/CloudKit/CKRecord-Extension.swift
+++ ZoneSharing/CloudKit/CKRecord-Extension.swift
@@ -8,25 +8,36 @@
 import Foundation
 import CloudKit
 
-extension CKRecord {
+public extension CKRecord {
     internal var genericID: String {
         return self.recordID.recordName
     }
 
     /// Extracts a required field value, throwing if the key is missing or the type doesn't match.
-    public func require<T>(_ key: String) throws -> T {
+    func require<T>(_ key: String) throws -> T {
         guard let value = self[key] as? T else {
-            throw AppError(record: self)
+            throw InvalidRecord(record: self)
         }
         
         return value
     }
-}
+    
+    internal struct InvalidRecord: LocalizedError {
+        let record: CKRecord
 
-struct AppError: Error {
-    let record: CKRecord
+        /// A localized message describing what error occurred.
+        var errorDescription: String? {
+            "Invalid record '\(record.recordType)' (\(record.genericID))"
+        }
+        
+        /// A localized message describing the reason for the failure.
+        var failureReason: String? { "Mismatching types" }
 
-    var errorDescription: String? {
-        "Invalid record '\(record.recordType)' (\(record.genericID))"
+        /// A localized message describing how one might recover from the failure.
+        var recoverySuggestion: String? { "Match your types" }
+
+        /// A localized message providing "help" text if the user requests help.
+        var helpAnchor: String? { "The CloudKit Record is missing a valid field" }
     }
+
 }
