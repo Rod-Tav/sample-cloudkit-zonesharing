ca8b5881e85b56e54777dbe1cb168aa61a17861d
Rod Tavangar <96751400+Rod-Tav@users.noreply.github.com>
2026-02-25 17:56:53 -0800

    not really sure why that was there, but my knowledge starts at ios 17. is this a bad change?

--- ZoneSharing/Models/Contact.swift
+++ ZoneSharing/Models/Contact.swift
@@ -6,20 +6,10 @@
 import Foundation
 import CloudKit
 
+/// A contact
 struct Contact: Identifiable {
     let id: String
     let name: String
     let phoneNumber: String
     let associatedStation: CKStation
-}
-
-extension Contact {
-    /// Initializes a `Contact` object from a CloudKit station.
-    /// - Parameter station: CloudKit station to pull values from.
-    init(station: CKStation) throws {
-        self.id = station.genericID
-        self.name = try station.require("name")
-        self.phoneNumber = try station.require("phoneNumber")
-        self.associatedStation = station
-    }
 }
